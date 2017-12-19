d1bfde6db3118b4297de195f4de37dd798c454be
Unknown <julio@megasoftgyn.com.br>
2017-12-18 23:47:20 -0200

    Adicao do RMI

--- src/br/com/st/controler/Cindex.java
+++ src/br/com/st/controler/Cindex.java
@@ -1,19 +1,34 @@
 package br.com.st.controler;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 import org.zkoss.zhtml.Link;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.ListModelArray;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Textbox;
 
+import br.com.diego.financas.modelo.Conta;
+import br.com.diego.financas.rmi.ContaRMIService;
+
+@VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class Cindex extends SelectorComposer<Component> {
 
+	
+	private static String CONTARMISERVICE_URL = "rmi://localhost:1099/ContaRMIService";
+	private static String MOVIMENTACAO_RMI_SERVICE_URL = "rmi://localhost:1099/ContaRMIService";
 	/**
 	 * Adicionar no controler o codigo abaixo
 	 */
@@ -28,6 +43,9 @@ public class Cindex extends SelectorComposer<Component> {
 
 	private ArrayList<LinkedHashMap<String, String>> listaOperacoes = new ArrayList<LinkedHashMap<String, String>>();
 
+	
+	
+	
 	// Metodo que ira ser acionado ao clicar no btnMaiuscula //
 	@Listen("onClick = #btnMaiuscula")
 	public void trocarPorMaiuscula() {
@@ -43,6 +61,22 @@ public class Cindex extends SelectorComposer<Component> {
 		hash.put("depois", depois); // O valor modificado
 		listaOperacoes.add(hash); // Adiciona a lista
 
+		
+		try {
+			ContaRMIService contaRMIService = (ContaRMIService) Naming.lookup(CONTARMISERVICE_URL);
+			List<Conta> contas = contaRMIService.getAllContas();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		this.lstbxOperacoesRealizadas.setModel(new ListModelArray<LinkedHashMap<String, String>>(this.listaOperacoes));
 	}
 
